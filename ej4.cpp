23681430bc1abc099c56c6d88e965433fb63b2df
miguelConchacalla <miguel.conchacalla@ucsp.edu.pe>
2022-09-18 23:41:15 -0500

    projecteuler 2

---
+++ ej4.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int palin(int n) {
+    for (long i = 1; i <= 99; i ++) {
+        for (long j = 1; j <= 99; j ++) {
+            long v1 = i * j;
+        }
+    }
+}
+
+int main()
+{
+    
+    return 0;
+}
